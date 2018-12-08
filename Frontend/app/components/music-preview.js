ca318cf3d57e5545ffa3bc2721b5c482c9892737
marsahrakorpi <markus.sahrakorpi@gmail.com>
2018-12-08 22:36:38 +0200

    Small fix to spotify api loading wrong songs

--- Frontend/app/components/music-preview.js
+++ Frontend/app/components/music-preview.js
@@ -17,29 +17,29 @@ export default Component.extend({
 
     setSource(){
         let track = this.get('spotify').getTrackSingle(this.get('trackName'))
-
+        let album = this.get('album')
+        
         track.then(track => {
             this.set('loading', true);
             this.set('spotifyRecordArray', track.tracks.items)
-            console.log(track);
-            let album = this.get('album')
+            
     
             for(var i = 0; i < this.get('spotifyRecordArray').length; i++) {
-                if (this.get('spotifyRecordArray')[i].album.name == album) {
+                if (this.get('spotifyRecordArray')[i].album.name.toLowerCase() === album.toLowerCase()) {
                     this.set('spotifyRecord', this.get('spotifyRecordArray')[i]);
                     break; 
                 }
             }
+
             if(!this.get('spotifyRecordArray')[0]){
                 this.set('loading', false)
                 return
             }
-            if(this.get('spotifyRecord')){
+            if(this.get('spotifyRecord')!=null){
                 this.set('source', this.get('spotifyRecord').preview_url);
                 
-            } else {
-                this.set('source', this.get('spotifyRecordArray')[0].preview_url);
             }
+
             this.set('loading', false)
         })
 
